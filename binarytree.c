77258e99248e01ea6664071ffce8429ffa32dafb
ljvalen <ljvalen@s991329.srn.sandia.gov>
2020-05-04 15:54:20 -0600

    Initial Commit

---
+++ binarytree.c
@@ -0,0 +1,214 @@
+/******************/
+/* Leroy Valencia */
+/*    04/20/20    */
+/*   CS-241-006   */
+/******************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "binarytree.h"
+
+struct TreeNode* createNode(int data)
+{
+  struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+
+  node->data = data;
+
+  node->left = NULL;
+  node->right = NULL;
+
+  return node;
+}
+
+struct TreeNode* insertBST(struct TreeNode* root, int data)
+{
+  /* If tree is empty then create new node */
+  if(root == NULL)
+  {
+    return createNode(data);
+  }
+  if( data < root-> data)
+  {
+    root->left = insertBST(root->left, data);
+  }
+  else if( data > root->data)
+  {
+    root->right = insertBST(root->right, data);
+  }
+  return root;
+}
+
+
+
+int removeBST(struct TreeNode** rootRef, int data)
+{
+  struct TreeNode* heriarchy;
+  struct TreeNode* previous;
+
+  if(*rootRef == NULL) return 0;
+
+  if(data < (*rootRef)->data)
+  {
+    return removeBST(&(*rootRef)->left, data);
+  }
+  else if(data > (*rootRef)->data)
+  {
+    return removeBST(&(*rootRef)->right, data);
+  }
+  else
+  {
+    struct TreeNode* temp;
+
+    if((*rootRef)->right == NULL)
+    {
+      temp = *rootRef;
+      *rootRef = (*rootRef)->left;
+      free(temp);
+    }
+    else if((*rootRef)->left == NULL)
+    {
+      temp = *rootRef;
+      *rootRef = (*rootRef)->right;
+      free(temp);
+    }
+    else
+    {
+      heriarchy = (*rootRef)->left;
+      previous = *rootRef;
+
+      while(heriarchy->right != NULL)
+      {
+        previous = heriarchy;
+        heriarchy = heriarchy->right;
+      }
+
+      (*rootRef)->data = heriarchy->data;
+
+      if(previous != *rootRef)
+      {
+        previous->right = heriarchy->left;
+      }
+      else
+      {
+        previous->left = heriarchy->left;
+      }
+
+      free(heriarchy);
+    }
+    return 1;
+  }
+  return 0;
+}
+
+
+int minValueBST(struct TreeNode* root)
+{
+  struct TreeNode* temp = root;
+
+  while(temp->left != NULL)
+  {
+    temp = temp->left;
+  }
+  return temp->data;
+}
+
+int maxDepth(struct TreeNode* root)
+{
+  if(root == NULL)
+  {
+    return 0;
+  }
+  else
+  {
+    int leftDepth = maxDepth(root->left);
+    int rightDepth = maxDepth(root->right);
+
+    return leftDepth > rightDepth ? leftDepth+1 : rightDepth+1;
+  }
+}
+
+int isBalanced(struct TreeNode* root)
+{
+  int leftHeight, rightHeight;
+  
+  if(root == NULL) return 1;
+
+  leftHeight = maxDepth(root->left);
+  rightHeight = maxDepth(root->right);
+
+  if(abs(leftHeight - rightHeight) <= 1 && isBalanced(root->left) && isBalanced(root->right))
+  {
+    return 1;
+  }
+
+  return 0;
+}
+
+int isBST(struct TreeNode* root)
+{
+  int min = -2147483647;
+ int max = 2147483647;
+  if(isBSTutil(root, min, max)) return 1;
+
+  return 0;
+}
+
+int isBSTutil(struct TreeNode* root, int minKey, int maxKey)
+{
+  if(root == NULL) return 1;
+
+  if(root-> data < minKey || root->data > maxKey) return 0;
+
+  return isBSTutil(root->left, minKey, root->data) &&
+          isBSTutil(root->right, root->data, maxKey);
+}
+
+void printTree(struct TreeNode* root)
+{
+  printTreeHelper(root);
+  printf("\n");
+}
+
+void printTreeHelper(struct TreeNode* root)
+{
+    if(root != NULL)
+  {
+    printTreeHelper(root->left);
+    printf("%d ", root->data);
+    printTreeHelper(root->right);
+  }
+}
+
+void printLeaves(struct TreeNode* root)
+{
+  printLeavesHelper(root);
+  printf("\n");
+}
+
+void printLeavesHelper(struct TreeNode* root)
+{
+  if(root == NULL) return;
+
+  if(!root->left && !root->right)
+  {
+    printf("%d ", root->data);
+    return;
+  }
+
+  if(root->left != NULL) printLeavesHelper(root->left);
+  if(root->right != NULL) printLeavesHelper(root->right);
+}
+
+
+void freeTree(struct TreeNode* root)
+{
+  if ( root != NULL)
+  {
+    freeTree(root->left);
+    freeTree(root->right);
+    free (root);
+  }
+}
---
+++ linkedlist.c
@@ -0,0 +1,156 @@
+/******************/
+/* Leroy Valencia */
+/*    04/20/20    */
+/*   CS-241-006   */
+/******************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "linkedlist.h"
+
+struct ListNode* createNode(int data)
+{
+  struct ListNode* list = (struct ListNode*)malloc(sizeof(struct ListNode*));
+  list->next = NULL;
+  return list;
+}
+
+struct ListNode* insertSorted(struct ListNode* head, int data)
+{
+  struct ListNode* result;
+  struct ListNode* current = &temp;
+}
+
+struct ListNode* sortedInsert(struct ListNode* head, int data)
+{
+  struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode*));
+  struct ListNode* temp;
+  struct ListNode* current = &temp;
+  newNode->data;
+  temp->next = head;
+
+  while(current->next != NULL && current->next->data < data)
+  {
+    current = current->next;
+  }
+  newNode->next = current->next;
+  current->next = newNode;
+  head = temp->next;
+}
+
+int removeItem(struct ListNode** headRef, int data)
+{
+  struct ListNode* temp = *headRef, *prev;
+
+  if(temp != NULL && temp->data == data)
+  {
+    *headRef = temp->next;
+    free(temp);
+    return 1;
+  }
+
+  while(temp != NULL && temp->data != data)
+  {
+    prev = temp;
+    temp = temp->next;
+  }
+
+  if (temp == NULL) return 0;
+
+  prev->next = temp->next;
+  free(temp);
+  return 1;
+}
+
+struct ListNode* pushStack(struct ListNode* head, int data)
+{
+  struct ListNode* temp = (struct ListNode*)malloc(sizeof(struct ListNode*));
+
+  if(temp == NULL) return head;
+
+  temp->data = data;
+  temp->next = head;
+
+  head = temp;
+  return head;
+}
+
+int popStack(struct ListNode** headRef)
+{
+  
+  if(*headRef != NULL)
+  {
+    struct ListNode* temp = *headRef;
+    *headRef = (*headRef)->next;
+    free(temp);
+  }
+  return (*headRef)->data;
+}
+
+int listLength(struct ListNode* head)
+{
+  int count;
+  count = 0;
+  struct ListNode* temp = head;
+  while(temp != NULL)
+  {
+    count++;
+    temp = temp->next;
+  }
+  return count;
+}
+
+void printList(struct ListNode* head)
+{
+  printListHelper(head);
+  printf("\n");
+}
+void printListHelper(struct ListNode* head)
+{
+  int count;
+  count = 0;
+  while(head != NULL)
+  {
+    if (count > 0)
+    {
+      printf(", %d", head->data);
+    }
+    else
+    {
+      printf("%d", head->data); 
+    }
+    head = head->next;
+    count++;
+  }
+}
+
+void freeList(struct ListNode* head)
+{
+  struct ListNode* temp;
+
+  while(head != NULL)
+  {
+    temp = head;
+    head = head->next;
+    free(temp);
+  }
+}
+
+void reverseList(struct ListNode** headRef)
+{
+  struct ListNode* previous = NULL;
+  struct ListNode* next = NULL;
+  struct ListNode* current = *headRef;
+
+  while(current != NULL)
+  {
+    next = current->next;
+    current->next = previous;
+    previous = current;
+    current = next;
+  }
+  *headRef = previous;
+}
